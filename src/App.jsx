08bd512e30a7c9303af30fd656ee031eebf339dd
Guilherme Henrique <guilhermeerba@hotmail.com>
2022-05-13 22:40:29 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import './App.css';
+import abi from './utils/abi.json';
+
+export default function App() {
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const contractAddress = "0x2EDF73f226D8974155a966808161A97Be77B5A7c";
+  const contractABI = abi.abi;
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log("Garanta que possua a Metamask instalada!");
+        return;
+      } else {
+        console.log("Temos o objeto ethereum", ethereum);
+      }
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Encontrada a conta autorizada:", account);
+        setCurrentAccount(account)
+      } else {
+        console.log("Nenhuma conta autorizada foi encontrada")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert("MetaMask encontrada!");
+        return;
+      }
+
+      const accounts = await ethereum.request({ method: "eth_requestAccounts" });
+
+      console.log("Conectado", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const wave = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let count = await wavePortalContract.getTotalWaves();
+        console.log("Recuperado o número de tchauzinhos...", count.toNumber());
+
+        /*
+        * Executar o tchauzinho a partir do contrato inteligente
+        */
+        const waveTxn = await wavePortalContract.wave();
+        console.log("Minerando...", waveTxn.hash);
+
+        await waveTxn.wait();
+        console.log("Minerado -- ", waveTxn.hash);
+
+        count = await wavePortalContract.getTotalWaves();
+        console.log("Total de tchauzinhos recuperado...", count.toNumber());
+      } else {
+        console.log("Objeto Ethereum não encontrado!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        🏄 Hey Guys!
+        </div>
+
+        <div className="bio">
+          I'm Gui and this is my first App on WEB3.
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Send Emoji
+        </button>
+
+        {/*
+        * Se não existir currentAccount, apresente este botão
+        */}
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
